5868b8e0bf0f29dd738d26a6e6bd86f247369ac4
Aimeri <edntremblay@gmail.com>
2023-06-04 05:51:13 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,12 +1,5 @@
 QBCore = exports['qb-core']:GetCoreObject()
 
-QBCore.Commands.Add('forcelock', 'Force open a locked vehicle', {}, false, function(source)
-    TriggerClientEvent('forcelocks:client:forcelock', source)
-end)
-
-RegisterNetEvent('forcelocks:server:forcelock', function(player, veh)
-    local src = source
-    TriggerClientEvent("forcelocks:client:breaklocks", source)
-
-
+QBCore.Functions.CreateUseableItem("blowtorch", function(source, item)
+        TriggerClientEvent('forcelocks:client:forcelock', source)
 end)
